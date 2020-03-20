077712495acf4d51f354952868cfb9e56bf1c8b8
Miroslav Zoricak <miroslav.zoricak@gmail.com>
2020-03-20 22:16:52 +0100

    Add basic server impl

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let checkin = warp::path!("v1" / "checkin").map(|| "checkin");
+    let app = checkin;
+
+    warp::serve(app)
+        .run(([127, 0, 0, 1], 3000)).await;
 }

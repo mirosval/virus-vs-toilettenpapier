9cf8b7b0d3f58ee62f1c8504cd7b2a83267170b1
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2020-03-20 23:38:21 +0100

    Copy paste template (#6)
    Co-authored-by: Bijan Chokoufe Nejad <bijan.nejad@numberfour.eu>

---
+++ client/src/Components/GeoLoc.svelte
@@ -0,0 +1,34 @@
+<script>
+let geoloc;
+
+function showLocation(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    geoloc = "Latitude : " + latitude + " Longitude: " + longitude;
+}
+
+function errorHandler(err) {
+    if (err.code == 1) {
+        alert("Error: Access is denied!");
+    } else if( err.code == 2) {
+        alert("Error: Position is unavailable!");
+    }
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        var options = {timeout:60000}; // timeout at 60 seconds
+        navigator.geolocation.getCurrentPosition(showLocation, errorHandler, options);
+    } else {
+        alert("Sorry, browser does not support geolocation!");
+    }
+}
+
+getLocation();
+
+</script>
+
+<main>
+	<p>It seems you are here: {geoloc}</p>
+	<img src="heatmap_dummy.png" alt="Heatmap dummy">
+</main>

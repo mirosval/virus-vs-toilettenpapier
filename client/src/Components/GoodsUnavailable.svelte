62e8fa87b4bf6a0d9bbe07850a1c0509f9c652ce
Bijan Chokoufe Nejad <bijan.nejad@numberfour.eu>
2020-03-21 20:28:34 +0100

    Add uncomitted file

---
+++ client/src/Components/GoodsUnavailable.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let GOODS_UNAVAILABLE;
+  import AutoComplete from "simple-svelte-autocomplete";
+  const items = [
+    {"id": 1, "name": "Toilettenpapier"},
+    {"id": 2, "name": "Spaghetti"},
+    {"id": 3, "name": "Penne"},
+    {"id": 4, "name": "Toast"},
+    {"id": 5, "name": "Brot"},
+    {"id": 6, "name": "Milch"}
+  ]
+  let selectedItem;
+</script>
+
+<div class="slidecontainer container">
+  <div class="row">
+    <div class="col">
+      {GOODS_UNAVAILABLE}
+    </div>
+    <div class="col">
+      <AutoComplete items={items} bind:selectedItem={selectedItem} labelFieldName="name" />
+    </div>
+  </div>
+</div>
+

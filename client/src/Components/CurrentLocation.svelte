f617a2eb3e1c5cd8d68d5bb820039b83d72f2c7c
andrew-secret <andre.rusakow@jimdo.com>
2020-03-22 15:07:50 +0100

    feat(client): add current location component

--- client/src/Components/CurrentLocation.svelte
+++ client/src/Components/CurrentLocation.svelte
@@ -1,12 +1,29 @@
 <script>
+  const url = "http://localhost:3000/v1/checkins/around";
   let geoloc;
-
+  function getNearbyCheckins(lat, lng) {
+    const payload = {
+      gps: [lat, lng],
+      radius: 1000,
+      offset: 0,
+      limit: 10
+    };
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    }).then(data => {
+      console.log(data);
+    });
+  }
   function showLocation(position) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
     geoloc = "Latitude : " + latitude + " Longitude: " + longitude;
+    getNearbyCheckins(latitude, longitude);
   }
-
   function errorHandler(err) {
     if (err.code == 1) {
       alert("Error: Access is denied!");
@@ -14,7 +31,6 @@
       alert("Error: Position is unavailable!");
     }
   }
-
   function getLocation() {
     if (navigator.geolocation) {
       var options = { timeout: 60000 }; // timeout at 60 seconds
@@ -27,7 +43,6 @@
       alert("Sorry, browser does not support geolocation!");
     }
   }
-
   getLocation();
 </script>
 
@@ -35,8 +50,23 @@
   class="light-color alert alert-light shadow-sm p-4"
   id="banner"
   role="alert">
-  <div class="container">
-    <h5 class="text-dark">Dein aktueller Standort:</h5>
+  <div class="container mb-4">
+    <h6 class="text-dark">Dein aktueller Standort:</h6>
     <p class="text-dark mb-0">{geoloc}</p>
   </div>
+  <form>
+    <div class="form-group mb-3">
+      <label class="text-dark" for="locationInput">Standort festlegen</label>
+      <input
+        type="text"
+        class="form-control form-control-lg"
+        id="locationInput"
+        placeholder="Altona Hauptbahnhof"
+        aria-describedby="location help" />
+    </div>
+
+    <button type="submit" class="btn btn-outline-dark btn-lg btn-block mb-3">
+      Submit
+    </button>
+  </form>
 </section>
